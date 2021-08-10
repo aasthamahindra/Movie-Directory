75aa0bfd5447a60fe6f1a0383d8b09ec55d84b11
Aastha Mahindra <69888214+aastha-m125@users.noreply.github.com>
2021-08-10 17:06:15 +0530

    Connected with the OMDB API to list movies by search.

--- app/src/main/java/com/example/moviedirectory/Activities/MainActivity.java
+++ app/src/main/java/com/example/moviedirectory/Activities/MainActivity.java
@@ -2,27 +2,53 @@ package com.example.moviedirectory.Activities;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.moviedirectory.Data.MovieRecyclerViewAdapter;
+import com.example.moviedirectory.Model.Movie;
 import com.example.moviedirectory.R;
+import com.example.moviedirectory.Util.Constants;
+import com.example.moviedirectory.Util.Prefs;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.moviedirectory.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private RecyclerView recyclerView;
+    private MovieRecyclerViewAdapter movieRecyclerViewAdapter;
+    private List<Movie> movieList;
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +59,31 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
+        queue = Volley.newRequestQueue(this);
+
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        Prefs prefs = new Prefs(MainActivity.this);
+        String search = prefs.getSearch();
+
+        movieList = new ArrayList<>();
+        getMovies(search);
+
+        movieRecyclerViewAdapter = new MovieRecyclerViewAdapter(this, movieList);
+        recyclerView.setAdapter(movieRecyclerViewAdapter);
+        movieRecyclerViewAdapter.notifyDataSetChanged();
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,5 +105,48 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public List<Movie> getMovies(String searchTerm) {
+        movieList.clear();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,Constants.URL_LEFT + searchTerm + Constants.URL_RIGHT + Constants.API_KEY, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray moviesArray = response.getJSONArray("Search");
+
+                    for(int i = 0; i < moviesArray.length(); i++) {
+                        JSONObject movieObj = moviesArray.getJSONObject(i);
+
+                        Movie movie = new Movie();
+
+                        movie.setTitle(movieObj.getString("Title"));
+                        movie.setYear("Year Released: " + movieObj.getString("Year"));
+                        movie.setMovieType("Type: " + movieObj.getString("Type"));
+                        movie.setPoster(movieObj.getString("Poster"));
+                        movie.setImdbId(movieObj.getString("imdbID"));
+
+                        Log.d("Movies: ", movie.getTitle());
+
+                        movieList.add(movie);
+                    }
+
+                    movieRecyclerViewAdapter.notifyDataSetChanged();
+
+                } catch(JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error: ", error.getMessage());
+            }
+        });
+
+        queue.add(jsonObjectRequest);
+
+        return movieList;
     }
 }
---
+++ app/src/main/java/com/example/moviedirectory/Util/Constants.java
@@ -0,0 +1,10 @@
+package com.example.moviedirectory.Util;
+
+public class Constants {
+
+    public static final String URL_LEFT = "http://www.omdbapi.com/?s=";
+    public static final String API_KEY = "&apikey=16eed72e";
+    public static final String URL_RIGHT = "&page=2";
+
+}
+
