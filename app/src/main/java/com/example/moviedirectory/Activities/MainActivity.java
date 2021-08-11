6feb29b11674256d7b24c77e7bfc96635529c97e
Aastha Mahindra <69888214+aastha-m125@users.noreply.github.com>
2021-08-11 18:40:56 +0530

    Added dialog to search different movies/series

--- app/src/main/java/com/example/moviedirectory/Activities/MainActivity.java
+++ app/src/main/java/com/example/moviedirectory/Activities/MainActivity.java
@@ -7,25 +7,20 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.moviedirectory.Data.MovieRecyclerViewAdapter;
 import com.example.moviedirectory.Model.Movie;
 import com.example.moviedirectory.R;
 import com.example.moviedirectory.Util.Constants;
 import com.example.moviedirectory.Util.Prefs;
-import com.google.android.material.snackbar.Snackbar;
 
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
 import android.view.View;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -33,6 +28,8 @@ import com.example.moviedirectory.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -49,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
     private MovieRecyclerViewAdapter movieRecyclerViewAdapter;
     private List<Movie> movieList;
     private RequestQueue queue;
+    private AlertDialog.Builder alertDialogBuilder;
+    private AlertDialog alertDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                showInputDialog();
             }
         });
 
@@ -72,10 +72,11 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        movieList = new ArrayList<>();
-
         Prefs prefs = new Prefs(MainActivity.this);
         String search = prefs.getSearch();
+
+        movieList = new ArrayList<>();
+
         //getMovies(search);
         movieList = getMovies(search);
 
@@ -99,17 +100,48 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.search_new) {
+            showInputDialog();
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    public void showInputDialog() {
+
+        alertDialogBuilder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_view, null);
+        final EditText newSearchEdt = (EditText) view.findViewById(R.id.searchEdt);
+        Button submitButton = (Button) view.findViewById(R.id.submitButton);
+
+        alertDialogBuilder.setView(view);
+        alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Prefs prefs = new Prefs(MainActivity.this);
+
+                if (!newSearchEdt.getText().toString().isEmpty()) {
+
+                    String search = newSearchEdt.getText().toString();
+                    prefs.setSearch(search);
+                    movieList.clear();
+
+                    getMovies(search);
+                }
+                alertDialog.dismiss();
+            }
+        });
+    }
+
     public List<Movie> getMovies(String searchTerm) {
         movieList.clear();
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,Constants.URL_LEFT + searchTerm + Constants.URL_RIGHT + Constants.API_KEY, null, new Response.Listener<JSONObject>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                Constants.URL_LEFT + searchTerm + Constants.URL_RIGHT + Constants.API_KEY,
+                null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
